6b6ac417e1fec78625a97abaa39912f3e0552676
husseinshaltout <48390004+husseinshaltout@users.noreply.github.com>
2020-06-02 11:08:14 +0200

    Create Def.py

---
+++ Def.py
@@ -0,0 +1,50 @@
+class Mixer(object):
+
+    def __init__(self, mixerSize, cleaningTime):
+        self.size = mixerSize
+        self.cT = cleaningTime
+
+class StorageTank(object):
+
+    def __init__(self, tankSize, cleaningTime):
+        self.size = tankSize
+        self.cT = cleaningTime
+
+class ProductionLine(object):
+    def __init__(self, plName, cleaningTime):
+        self.name = plName
+        self.cT= cleaningTime
+
+class Product(Mixer):
+    machines = []
+    def __init__(self, mL, pName, mixerNo, productionLine, COT, netSpeed, headCount, cycleTime, isOutSourced):
+        self.machines = mL
+        self.name = pName
+        self.MNO = mixerNo
+        self.line = productionLine
+        self.cot  = COT #Change over time
+        self.speed = netSpeed #Net speed
+        self.heads = headCount
+        self.cTime = cycleTime #Cycle time
+        self.totaltime = self.cot + self.cTime + self.MNO.cT + self.line.cT
+mixer_2 = Mixer(2,1.5)
+mixer_1 = Mixer(0.5,1.5)
+
+storagetank_1 = StorageTank(2,1.5)
+storagetank_2 = StorageTank(2,1.5)
+storagetank_3 = StorageTank(2,1.5)
+storagetank_4 = StorageTank(2,1.5)
+storagetank_5 = StorageTank(2,1.5) 
+storagetank_6 = StorageTank(2,1.5)
+storagetank_7 = StorageTank(2,1.5)
+storagetank_8 = StorageTank(2,1.5)
+
+Rollon_PL = ProductionLine("Rollon",1)
+Tubes_PL = ProductionLine("Tubes",1)
+Cream1_PL = ProductionLine("Cream 1",1)
+Cream2_PL = ProductionLine("Cream 2",1)
+
+Ferrari = Product("Mixer 2", "Ferrari1", mixer_2, Cream1_PL, 1.5, 32, 8, 6, False)
+StarWors = Product("Mixer 1", "Cream Star wors20", mixer_1, Cream2_PL, 1.5, 44, 8, 0, True)
+jobs = (Ferrari, StarWors)
+# print(jobs[0].totaltime, jobs[0].cot, jobs[0].cTime, jobs[0].MNO.cT, jobs[0].line.cT)
