4d2111aaed26ea72314f9204c6ba977880c28143
husseinshaltout <48390004+husseinshaltout@users.noreply.github.com>
2020-06-03 00:17:33 +0200

    Update Def.py

--- Def.py
+++ Def.py
@@ -29,20 +29,21 @@ class Product(Mixer):
         self.heads = headCount
         self.cTime = cycleTime #Cycle time
         self.totaltime = self.cot + self.cTime + self.MNO.cT + self.line.cT
+        self.isOutSourced = isOutSourced
     def __repr__(self):
         return str(self.name)
 
 mixer_2 = Mixer(2,1.5)
-mixer_1 = Mixer(0.5,1.5)
+# mixer_1 = Mixer(0.5,1.5)
 
 storagetank_1 = StorageTank("StorageTank 1", 2, 1.5)
-storagetank_2 = StorageTank("StorageTank 2", 2, 1.5)
-storagetank_3 = StorageTank("StorageTank 3", 2, 1.5)
-storagetank_4 = StorageTank("StorageTank 4", 2, 1.5)
-storagetank_5 = StorageTank("StorageTank 5", 2, 1.5)
-storagetank_6 = StorageTank("StorageTank 6", 2, 1.5)
-storagetank_7 = StorageTank("StorageTank 7", 2, 1.5)
-storagetank_8 = StorageTank("StorageTank 8", 2, 1.5)
+# storagetank_2 = StorageTank("StorageTank 2", 2, 1.5)
+# storagetank_3 = StorageTank("StorageTank 3", 2, 1.5)
+# storagetank_4 = StorageTank("StorageTank 4", 2, 1.5)
+# storagetank_5 = StorageTank("StorageTank 5", 2, 1.5)
+# storagetank_6 = StorageTank("StorageTank 6", 2, 1.5)
+# storagetank_7 = StorageTank("StorageTank 7", 2, 1.5)
+# storagetank_8 = StorageTank("StorageTank 8", 2, 1.5)
 
 Rollon_PL = ProductionLine("Rollon",1)
 Tubes_PL = ProductionLine("Tubes",1)
@@ -50,5 +51,19 @@ Cream1_PL = ProductionLine("Cream 1",1)
 Cream2_PL = ProductionLine("Cream 2",1)
 
 Ferrari = Product("Mixer 2", "Ferrari1", mixer_2, Cream1_PL, 1.5, 32, 8, 6, False)
-StarWors = Product("Mixer 1", "Cream Star wors20", mixer_1, Cream2_PL, 1.5, 44, 8, 0, True)
-jobs = (Ferrari, StarWors)
+StarWors = Product("Mixer 1", "Cream Star wors20", mixer_2, Cream2_PL, 1.5, 44, 8, 0, True)
+jobs = [Ferrari, StarWors]
+print(Ferrari.name,Ferrari.cTime + Ferrari.MNO.cT +Ferrari.cot)
+print(StarWors.name,StarWors.cTime + StarWors.MNO.cT + StarWors.cot)
+Tobestored = []
+StorageTanks = [storagetank_1,storagetank_1]
+'''
+            #    8  9   10  11  12  01  02  03  
+Mchromosome = [ [Ferrari], [Ferrari], [Ferrari], [Ferrari], [Ferrari], [Ferrari], [], [], [], [], [], [], #Sunday
+               [], [], [], [], [], [], [], [], [Ferrari], [], [], [], #Monday
+               [], [], [], [], [], [], [], [], [Ferrari], [], [], [] , #Tuesday
+               [], [], [], [], [], [], [], [], [Ferrari], [], [], [] , #Wednesday
+               [], [], [], [], [], [], [], [] , [Ferrari], [], [], [] ] #Thursday
+
+          
+'''
